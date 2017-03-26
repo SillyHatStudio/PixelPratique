ad304c4b8ed07f6095ee5e136b4270fd5f56fc78
yannismrad <yannismrad@free.fr>
2017-03-26 19:20:07 -0400

    TimerManager + folders to organize code

---
+++ Assets/Scripts/GameManagement/TimerManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pixel.Game.Management
+{
+    /** 
+     * Time Management (countdown timer & regular timer)
+     **/
+    public class TimerManager : MonoBehaviour
+    {
+        public Text timerTextObject;
+        public EnumTypes.TimerTypeEnum m_timerType;
+
+        private float timer;
+        private float elapsedMins, elapsedSecs;
+
+        //(variables for countdown timer)
+        [Range(0, 59)]
+        public int countDownMinutes;
+        [Range(0, 59)]
+        public int countDownSeconds;
+        private float remainingMins, remainingSecs, milliseconds;
+        private bool timerOver;
+
+        private void Awake()
+        {
+            if (m_timerType == EnumTypes.TimerTypeEnum.Normal)
+            {
+                elapsedMins = 0;
+                elapsedSecs = 0;
+            }
+
+            if (m_timerType == EnumTypes.TimerTypeEnum.Countdown)
+            {
+                remainingMins = countDownMinutes;
+                remainingSecs = countDownSeconds;
+                milliseconds = 100;
+                timerOver = false;
+            }
+        }
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+            //Compute & display elapsed time (normal timer)
+            if (m_timerType == EnumTypes.TimerTypeEnum.Normal)
+            {
+                timer += Time.deltaTime;
+
+                elapsedMins = Mathf.Floor(timer / 60);
+                elapsedSecs = timer % 60;
+
+                if (timerTextObject != null)
+                {
+                    timerTextObject.text = elapsedMins.ToString("00") + ":" + elapsedSecs.ToString("00");
+                }
+            }
+
+            //Compute & display  remaining time (countdown timer)
+            if (m_timerType == EnumTypes.TimerTypeEnum.Countdown)
+            {
+                if (remainingMins <= 0 && remainingSecs <= 0 && milliseconds <= 0)
+                {
+                    //Timer ends
+                    //Add some code here when the timer finishes
+                    timerOver = true;
+                }
+
+                else
+                {
+                    if (!timerOver)
+                    {
+                        if (milliseconds <= 0)
+                        {
+                            if (remainingSecs <= 0)
+                            {
+                                remainingMins--;
+                                remainingSecs = 59;
+
+                            }
+
+                            else if (remainingSecs >= 1)
+                            {
+                                remainingSecs--;
+                            }
+
+                            milliseconds = 100;
+                        }
+
+                        milliseconds -= Time.deltaTime * 100;
+
+                        if (timerTextObject != null)
+                        {
+                            if (remainingSecs < 10)
+                            {
+                                timerTextObject.text = string.Format("{0}:0{1}", remainingMins, remainingSecs);
+                            }
+
+                            else
+                                timerTextObject.text = string.Format("{0}:{1}", remainingMins, remainingSecs);
+                        }
+
+                        Debug.Log("rem tine " + remainingMins + ":" + remainingSecs);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Global/EnumTypes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pixel.Game.Management
+{
+    /**
+    * List of Enums usable from any script
+    **/
+    public class EnumTypes
+    {
+        //Time management
+        public enum TimerTypeEnum
+        {
+            Countdown = 0, //Timer that starts from XX:XX ends at 00:00
+            Normal = 1 // Timer that starts from 00:00
+        }
+    }
+}
+   
