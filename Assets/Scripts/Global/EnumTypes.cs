52a8c7f4edbb5d61af1cd077d7805fa256c15c79
yannismrad <yannismrad@free.fr>
2017-03-28 22:06:57 -0400

    Pacman player movements

--- Assets/Scripts/Global/EnumTypes.cs
+++ Assets/Scripts/Global/EnumTypes.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-namespace Pixel.Game.Management
-{
     /**
     * List of Enums usable from any script
     **/
     public class EnumTypes
+{
+    //Time management
+    public enum TimerTypeEnum
     {
-        //Time management
-        public enum TimerTypeEnum
-        {
-            Countdown = 0, //Timer that starts from XX:XX ends at 00:00
-            Normal = 1 // Timer that starts from 00:00
-        }
+        Countdown = 0, //Timer that starts from XX:XX ends at 00:00
+        Normal = 1 // Timer that starts from 00:00
+    }
+
+    //Player Numbers
+    public enum PlayerEnum
+    {
+        Unassigned = -1,
+        P1 = 0,
+        P2,
+        P3,
+        P4
+    }
+
+    //PlayerControlTypes
+    public enum PlayerControlKeys
+    {
+        P1_Up = KeyCode.W,
+        P1_Down = KeyCode.S,
+        P1_Left = KeyCode.A,
+        P1_Right = KeyCode.D,
+        P2_Up = KeyCode.UpArrow,
+        P2_Down = KeyCode.DownArrow,
+        P2_Left = KeyCode.LeftArrow,
+        P2_Right = KeyCode.RightArrow,
+        P3_Up = KeyCode.Y,
+        P3_Down = KeyCode.H,
+        P3_Left = KeyCode.G,
+        P3_Right = KeyCode.J,
+        P4_Up = KeyCode.O,
+        P4_Down = KeyCode.L,
+        P4_Left = KeyCode.K,
+        P4_Right = KeyCode.Semicolon
     }
 }
-   
+
+
